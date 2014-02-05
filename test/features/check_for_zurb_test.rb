ae2d51dbdf22c2303390179c7d772d07f2d29392
Dave Lane <dlane@whitepages.com>
2014-02-05 08:01:29 -0800

    Updated zurb test. Use dev assets in test environment

--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -3,7 +3,9 @@ require "test_helper"
 feature "Check for zurb foundation framework" do
   scenario "On page load" do
     visit new_post_path
-    page.has_css?('.columns')
+    # page.has_css?('.columns')
+    page.source.must_include("foundation_and_overrides.css")
+    page.source.must_include("foundation.js")
   end
 end
 
